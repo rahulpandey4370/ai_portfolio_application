a123013997398ceb726eb4ce33d30e0f99fcc89f
rahulrock.bgp <rahulrock.bgp@gmail.com>
2025-05-14 19:03:37 +0000

    fix: cv source fixed

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -114,7 +114,7 @@ export default function HeroSection() {
               </Link>
             </Button>
             <Button asChild variant="outline" size="lg" className="shadow-lg hover:shadow-accent/30 transition-shadow w-full sm:w-auto">
-              <a href="/Rahul_Ranjan_Pandey_Resume_AI_Portfolio.pdf" download="Rahul_Ranjan_Pandey_Resume_AI_Portfolio.pdf">
+              <a href="/Rahul_Ranjan_Pandey_Resume_AI_ML_Data.pdf" download="Rahul_Ranjan_Pandey_Resume_AI_ML.pdf">
                 Download CV
                 <Download className="ml-2 h-5 w-5" />
               </a>
